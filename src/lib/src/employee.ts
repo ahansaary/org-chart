2d699a364f35b1d95b0780b575ba6313479c124d
Ahmed Al Ansaary <ahansaary@gmail.com>
2018-02-24 17:35:16 +0200

    feat: Added expand / collapse feature

--- src/lib/src/employee.ts
+++ src/lib/src/employee.ts
@@ -1,12 +1,13 @@
 export interface IEmployee {
-	name: string;
-	designation: string;
-	subordinates: IEmployee[];
+        name: string;
+        designation?: string;
+        subordinates: IEmployee[];
+        collapsed?: boolean;
 }
 
 export class Employee implements IEmployee {
 	name: string;
-	designation: string;
+	designation?: string;
 	subordinates: Employee[];
 	manager?: Employee;
 	constructor(orgStructure: string[], manager?: Employee) {
